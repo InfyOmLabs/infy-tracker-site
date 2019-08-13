121c7bb6077963bcbe90c47ddd0487c0421f0eb8
Mitul Golakiya <mtl.golakiya@gmail.com>
2019-08-13 17:00:28 +0530

    docs: new docs added for installation

---
+++ assets/js/6.5f909de9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{178:function(n,w,o){}}]);
--- assets/js/6.901883e3.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{179:function(n,w,o){}}]);
